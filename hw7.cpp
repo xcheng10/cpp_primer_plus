f8c22e0920730a043359e710225b29cd1e6912a5
xcheng10 <chengxingchang@hotmail.com>
2021-02-22 17:17:40 -0800

    HW 7

--- hw7.cpp
+++ hw7.cpp
@@ -7,14 +7,33 @@
 #include <cstdlib>
 
 using namespace std;
-void hwc7();
+double calculate(double, double, double (*)(double, double));
+double add(double, double);
+double subtract(double, double);
 
 int main() {
-    hwc7();
-    return 0;
-};
-
-void hwc7(){
     cout << "Chapter 7 Homework" << endl;
+    double (*pf[2])(double, double) = {add, subtract};
+    double input1;
+    cin >> input1;
+    double input2;
+    cin >> input2;
+    for (int i = 0; i < 2; i++){
+        cout << calculate(input1, input2, pf[i]) << endl;
+    };
+    return 0;
 }
 
+double calculate(double x, double y, double (*pf1)(double, double)){
+    double q = pf1(x, y);
+    // cout << q << endl;
+    return q;
+}
+
+double add(double x, double y){
+    return x + y;
+}
+
+double subtract(double x, double y){
+    return x - y;
+}
