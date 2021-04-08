e1de1e03dfe3ee87b488915b84c260c4b45889db
xcheng10 <chengxingchang@hotmail.com>
2021-04-07 20:42:09 -0400

    HW 8

--- HW 8.cpp
+++ HW 8.cpp
@@ -1,8 +1,11 @@
+// https://github.com/AdamYuWen/CPP_Primer_Plus
+
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-struct box{
+struct box {
     char maker[40];
     float height;
     float width;
@@ -11,15 +14,38 @@ struct box{
     /* data */
 };
 
+struct stringy {
+    char* str;
+    int ct;
+};
+
 void display(const box& b);
 box& volume(box& b);
 
+
+void show(const stringy& _str, int n = 1);
+void show(const char* _str, int n = 1);
+void set(stringy& _stry, char _char[]);
+
+
 int main(){
-    box one = {"gucci", 9.0, 2.1, 8.8};
-    display(one);
-    volume(one);
-    display(one);
-    cout << "Homework 8" << endl;
+    // box one = {"gucci", 9.0, 2.1, 8.8};
+    // display(one);
+    // volume(one);
+    // display(one);
+    // cout << "Homework 8" << endl;
+
+    stringy beany;
+    char testing[] = "Reality isn't what it used to be.";
+
+    set(beany, testing);
+    show(beany);
+    show(beany, 2);
+    testing[0] = 'D';
+    testing[1] = 'u';
+    show(testing);
+    show(testing, 3);
+    show("Done!");
     return 0;
 }
 
@@ -35,3 +61,29 @@ box& volume(box& b){
     b.volume = b.height * b.length * b.width;
     return b;
 }
+
+void set(stringy& _stry, char _char[]){
+    _stry.ct = strlen(_char);
+    _stry.str = new char[_stry.ct + 1];
+    strcpy(_stry.str, _char);
+}
+
+
+void show(const stringy& _str, int n){
+    for (int i = 0; i < n; ++i){
+        cout << _str.str << endl;
+        cout << _str.ct << endl;        
+    }
+
+}
+
+void show(const char* _str, int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; _str[j] != '\0'; j++){
+            cout << _str[j];
+        }
+        cout << endl;
+    }
+}
+
+
