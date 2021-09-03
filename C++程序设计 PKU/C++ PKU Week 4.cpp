a5a182ec31bab39027e7adbbbfa9d002d51b6f38
xcheng10 <chengxingchang@hotmail.com>
2021-09-03 00:33:17 -0400

    test CLion

--- C++程序设计 PKU/C++ PKU Week 4.cpp
+++ C++程序设计 PKU/C++ PKU Week 4.cpp
@@ -202,7 +202,22 @@ class Complex{
     }
 };
 
-
+class MyInt {
+    int nVal;
+public:
+    MyInt(int n) { nVal = n; }
+    int ReturnVal() { return nVal; }
+// 在此处补充你的代码
+};
+int main () {
+    MyInt objInt(10);
+    objInt-2-1-3;
+    cout << objInt.ReturnVal();
+    cout <<",";
+    objInt-2-1;
+    cout << objInt.ReturnVal();
+    return 0;
+}
 
 
 
